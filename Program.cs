5c6b4db70c9b9973378d970db5a01afdc3047a84
reb1t <fgkkhbgf@gmail.com>
2026-08-14 19:16:27 +0500

    Добавлен запуск приложений по текстовым командам / Added application launching via text commands

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+Console.Write("Введите команду: ");
+string? command = Console.ReadLine()?.ToLower();
+if (command == "открой steam")
+{
+    Process.Start("C:\\Program Files (x86)\\Steam\\steam.exe");
+    Console.WriteLine("Steam открыт!");
+}
+else if (command == "открой zen")
+{
+    Process.Start("C:\\Program Files\\Zen Browser\\zen.exe");
+    Console.WriteLine("Zen Browser открыт!");
+}
+else
+{
+    Console.WriteLine("Команда не распознана.");
+}
