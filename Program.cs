00fa14c55c1b62a194e7b79fee2079e5e283770d
reb1t <fgkkhbgf@gmail.com>
2026-08-14 19:26:44 +0500

    Добавлен цикл обработки команд и команда выхода / Added command loop and exit command

--- Program.cs
+++ Program.cs
@@ -1,18 +1,26 @@
 ﻿using System.Diagnostics;
 
-Console.Write("Введите команду: ");
-string? command = Console.ReadLine()?.ToLower();
-if (command == "открой steam")
+while (true)
 {
-    Process.Start("C:\\Program Files (x86)\\Steam\\steam.exe");
-    Console.WriteLine("Steam открыт!");
-}
-else if (command == "открой zen")
-{
-    Process.Start("C:\\Program Files\\Zen Browser\\zen.exe");
-    Console.WriteLine("Zen Browser открыт!");
-}
-else
-{
-    Console.WriteLine("Команда не распознана.");
+    Console.Write("Введите команду: ");
+    string? command = Console.ReadLine()?.ToLower();
+    if (command == "открой steam")
+    {
+        Process.Start("C:\\Program Files (x86)\\Steam\\steam.exe");
+        Console.WriteLine("Steam открыт!");
+    }
+    else if (command == "открой zen")
+    {
+        Process.Start("C:\\Program Files\\Zen Browser\\zen.exe");
+        Console.WriteLine("Zen Browser открыт!");
+    }
+    else if (command == "выход")
+    {
+        Console.WriteLine("Выход из программы...");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Команда не распознана.");
+    }
 }
